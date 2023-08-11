938e138685f11d13a8fe85dc9345ebbae6cda521
Ben <100096813+BenRomeroArg@users.noreply.github.com>
2023-08-11 12:48:27 -0300

    prueba v1.1

---
+++ src/etapa3guia2e2/Etapa3guia2E2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etapa3guia2e2;
+
+/**
+ *
+ * @author HP
+ */
+public class Etapa3guia2E2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Convertir c = new Convertir();
+        
+        c.setVisible(true);
+       
+    }
+    
+}
